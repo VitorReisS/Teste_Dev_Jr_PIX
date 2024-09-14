22013629e1697c274dd27d8dcc7b3eed2a1825e6
VitorReisS <vitor20101983@gmail.com>
2024-09-14 08:58:05 -0300

    Add Models Agendamento

--- pagamentos/agendamentos/models.py
+++ pagamentos/agendamentos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Agendamento(models.Model):
+    data_pagamento = models.DateField()
+    permite_recorrencia = models.BooleanField()
+    quantidade_recorrencia = models.IntegerField()
+    intervalo_recorrencia = models.IntegerField()
+    status_recorrencia = models.CharField(max_length=100)
+    agencia = models.IntegerField()
+    conta = models.IntegerField()
+    valor_pagamento = models.IntegerField()  # Armazena o valor como inteiro
+
+    def save(self, *args, **kwargs):
+        # Converte o valor do pagamento de decimal para inteiro antes de salvar
+        if isinstance(self.valor_pagamento, float):
+            self.valor_pagamento = int(self.valor_pagamento * 100)  # Exemplo de conversão para centavos
+        super().save(*args, **kwargs)
