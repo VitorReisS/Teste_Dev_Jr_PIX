ea7d30b8c7b168a9e68a4db11ccbb75df6f44ac8
VitorReisS <vitor20101983@gmail.com>
2024-09-14 10:20:21 -0300

    Configurar URLs para o App Agendamentos

--- pagamentos/agendamentos/urls.py
+++ pagamentos/agendamentos/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('agendamentos/', views.listar_agendamentos, name='listar_agendamentos'),  # GET
     path('agendamentos/<int:id>/', views.consultar_agendamento, name='consultar_agendamento'),  # GET
     path('agendamentos/<int:id>/deletar/', views.deletar_agendamento, name='deletar_agendamento'),  # DELETE
+    path('agendamentos/<int:id>/atualizar/', views.atualizar_agendamento, name='atualizar_agendamento'), # PATCH
 ]
