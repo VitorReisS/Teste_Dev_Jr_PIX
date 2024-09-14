3feefe923ed14cebc006b55196d2cbf366ab06b2
VitorReisS <vitor20101983@gmail.com>
2024-09-14 09:35:06 -0300

    Configurar URLs para o App Agendamentos, incluindo endpoints para operações CRUD

--- pagamentos/agendamentos/urls.py
+++ pagamentos/agendamentos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('agendamentos/criar/', views.criar_agendamento, name='criar_agendamento'),  # POST
     path('agendamentos/', views.listar_agendamentos, name='listar_agendamentos'),  # GET
     path('agendamentos/<int:id>/', views.consultar_agendamento, name='consultar_agendamento'),  # GET
+    path('agendamentos/<int:id>/deletar/', views.deletar_agendamento, name='deletar_agendamento'),  # DELETE
 ]
